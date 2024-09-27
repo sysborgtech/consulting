8bf6833268a5f8d2863671a0fa4c0a466de8b6a6
sysborgtech <sysborgtech@gmail.com>
2024-09-27 15:25:07 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('.owl-1').owlCarousel({
+    loop:false,
+    margin: 10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: false,
+    dots: false,
+    autoplay:true,
+    items: 6,
+    margin: 30,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+
